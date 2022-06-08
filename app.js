1a5ac49d972a11998f5bcf796fe6815c82877621
ThomasTejeda <tejedamatiasthomas98@gmail.com>
2022-06-08 13:43:03 -0300

    Actualizo contacto.html

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ let port = 3000
 
 app.get("/", (req, res) => res.send("Bienvenidos a nuestra pagina"))
 app.get("/home",(requ,res) => res.sendFile(path.join(__dirname,"views","contacto","home.html")))
+
 app.get("/contacto",(requ,res) => res.sendFile(path.join(__dirname,"views","contacto.html")))
 
 app.listen(port, () => console.log(`Servidor levantado con exito en http://localhost:${port}`))
