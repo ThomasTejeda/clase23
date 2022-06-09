86733d3da8ec81fc54f11e51242b585b16ba70f0
ThomasTejeda <tejedamatiasthomas98@gmail.com>
2022-06-09 16:12:07 -0300

    Cambios agregados de la clase 24

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-let express = require("express")
-let path = require("path")
+let express = require('express')
+let path = require('path')
 let app = express()
 let port = 3000
 
+app.use(express.static(path.join(__dirname,'public')))
 
+/* app.get('/', (req,res) => res.send("Bienvenidos a nuestra pagina")) */
+app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')))
 
-app.get("/", (req, res) => res.send("Bienvenidos a nuestra pagina"))
-app.get("/home",(requ,res) => res.sendFile(path.join(__dirname,"views","contacto","home.html")))
+app.get('/contacto',(req,res) => res.sendFile(path.resolve(__dirname,'views','contacto.html')))
 
-app.get("/contacto",(requ,res) => res.sendFile(path.join(__dirname,"views","contacto.html")))
+app.get('/login',(req,res) => res.sendFile(path.resolve(__dirname,'views','login.html')))
+
+app.get('/register',(req,res)=> res.sendFile(path.resolve(__dirname,'views','register.html')))
 
 app.listen(port, () => console.log(`Servidor levantado con exito en http://localhost:${port}`))
